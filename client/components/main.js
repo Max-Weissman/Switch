111da7bdc94d2f50540fdaa0cad00b14c1a7d679
Max <maxweissman@rogers.com>
2022-10-05 15:09:56 -0400

    Made waterwheel slider

--- client/components/main.js
+++ client/components/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const array = ['2.jpg','3.jpg','4.jpg','4.jpg','2.jpg','3.jpg']
 let move = 0
+let clicked = false
 
 export default class Main extends React.Component{
     constructor() {
@@ -9,23 +10,42 @@ export default class Main extends React.Component{
         this.state = {
             move: 0
         }
-        this.click = this.click.bind(this)
+        this.scroll = this.scroll.bind(this)
     }
 
-    click (event) {
-        console.log(event.movementX)
-        move += event.movementX
-        this.setState({move: 1})
+    click () {
+        clicked = true
+    }
+
+    unclick () {
+        clicked = false
+    }
+
+    scroll (event) {
+        event.preventDefault()
+        if (clicked){
+            move += event.movementX
+            if (move > 49){
+                move = 0
+                let shift = array.pop()
+                array.unshift(shift)
+            }
+            if (move < -49){
+                move = 0
+                let shift = array.shift()
+                array.push(shift)
+            }
+            this.setState({move: 1})
+        }
     }
 
     render(){
-        return (<div onMouseMove={this.click}>
+        return (<div className='waterwheel' onMouseMove={this.scroll} onMouseDown={this.click} onMouseUp={this.unclick} onMouseLeave={this.unclick}>
             <img src={array[0]} width="50" height="50" style={{"translate": move + "px"}}></img>
             <img src={array[1]} width="50" height="50" style={{"translate": move + "px"}}></img>
             <img src={array[2]} width="50" height="50" style={{"translate": move + "px"}}></img>
             <img src={array[3]} width="50" height="50" style={{"translate": move + "px"}}></img>
             <img src={array[4]} width="50" height="50" style={{"translate": move + "px"}}></img>
-            <img src={array[5]} width="50" height="50" style={{"translate": move + "px"}}></img>
         </div>);
     }
 }
